8f55fa0160cb749fd708db4eb9cdc93733b42e4d
Rohit Gurnani <rkgi10@gmail.com>
2015-07-13 22:48:02 +0530

    Initial Commit

---
+++ NLPDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NLPDemo
+//
+//  Created by Rohit Gurnani on 13/07/15.
+//  Copyright (c) 2015 Rohit Gurnani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NLPDemoTests/NLPDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  NLPDemoTests.swift
+//  NLPDemoTests
+//
+//  Created by Rohit Gurnani on 13/07/15.
+//  Copyright (c) 2015 Rohit Gurnani. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NLPDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
